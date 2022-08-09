bcd5b82514b170f38d3af760586693751250f26f
Nana Kwesi Ofosu-Aikins <nanaaikinson2020@gmail.com>
2022-08-09 21:52:41 +0000

    Chat bubbles

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,10 +1,24 @@
 import { FC, FormEvent, useState } from "react";
+import { Avatar } from "./common/Avatar";
 import { Button } from "./common/Button";
 import { Input } from "./common/Input";
 
 export const Chat: FC = () => {
   // Data
   const [message, setMessage] = useState<string>("");
+  const messages = [
+    { id: 1, me: true, text: "JA" },
+    { id: 2, me: true, text: "JA" },
+    { id: 3, me: false, text: "NO" },
+    { id: 4, me: true, text: "JA" },
+    { id: 5, me: false, text: "EB" },
+    { id: 6, me: false, text: "EB" },
+    { id: 7, me: false, text: "EB" },
+    { id: 8, me: false, text: "EB" },
+    { id: 9, me: false, text: "EB" },
+    { id: 10, me: false, text: "EB" },
+    { id: 11, me: false, text: "EB" },
+  ];
 
   // Methods
   const submitMessage = (e: FormEvent) => {
@@ -22,6 +36,25 @@ export const Chat: FC = () => {
             className="flex w-full h-screen border border-gray-200"
             style={{ contain: "content" }}
           >
+            <div className="px-5 py-10 space-y-8 h-full w-full overflow-y-auto">
+              {messages.map((record, index) => (
+                <div
+                  className={
+                    record.me
+                      ? "flex flex-row-reverse space-x-2 space-x-reverse"
+                      : "flex flex-row space-x-2"
+                  }
+                  key={index}
+                >
+                  <Avatar text={record.text} />
+                  <div className="bg-gray-200 rounded-lg p-4 mt-8">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Eveniet, magni?
+                  </div>
+                </div>
+              ))}
+            </div>
+
             <footer className="fixed bottom-0 w-full py-3 px-5 bg-gray-100">
               <form onSubmit={submitMessage}>
                 <div className="flex items-center space-x-2">
